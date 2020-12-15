9c77ffe8fac66ba1b7f52a019d4035ec95d0dad8
Taylor Niles <tniles320@gmail.com>
2020-12-15 12:41:34 -0800

    updating index.html and service-worker, adding db.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,16 +3,20 @@ const DATA_CACHE_NAME = "data-cache-v1";
 
 const FILES_TO_CACHE = [
   "/",
+  "/db.js",
   "/index.html",
+  "/index.js",
   "/manifest.webmanifest",
   "/style.css",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
 ];
 
 self.addEventListener("install", function (evt) {
   evt.waitUntil(
-    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/images"))
+    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
   );
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -35,33 +39,36 @@ self.addEventListener("activate", function (evt) {
       );
     })
   );
-  self.ClientRectList.claim();
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", function (evt) {
-  if (evt.reqeust.url.includes("/api/")) {
+  if (evt.request.url.includes("/api/")) {
     evt.respondWith(
-      caches.open(DATA_CACHE_NAME).then((cache) => {
-        return fetch(evt.request)
-          .then((response) => {
-            if (response.status === 200) {
-              cache.put(evt.request.url, response.clone());
-            }
-            return response;
-          })
-          .catch((err) => {
-            return cache.match(evt.request);
-          });
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(evt.request)
+            .then((response) => {
+              if (response.status === 200) {
+                cache.put(evt.request, response.clone());
+              }
+
+              return response;
+            })
+            .catch((err) => {
+              return cache.match(evt.request);
+            });
+        })
+        .catch((err) => console.log(err))
+    );
+  } else {
+    evt.respondWith(
+      caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(evt.request).then((response) => {
+          return response || fetch(evt.request);
+        });
       })
     );
   }
-  return;
 });
-
-evt.respondWith(
-  caches.open(CACHE_NAME).then((cache) => {
-    return cache.match(evt.reqeust).then((response) => {
-      return response || fetch(evt.request);
-    });
-  })
-);
