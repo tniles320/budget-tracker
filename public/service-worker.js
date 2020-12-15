a6eb6c4fc18d9e3b8e1d4a859b2d9ab3dccaaa4f
Taylor Niles <tniles320@gmail.com>
2020-12-15 13:51:56 -0800

    reverting service-worker, updating index.html

--- public/service-worker.js
+++ public/service-worker.js
@@ -16,25 +16,15 @@ const FILES_TO_CACHE = [
 
 self.addEventListener("install", function (evt) {
   evt.waitUntil(
+    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
+  );
+  evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log("Your files were pre-cached successfully!");
-      return cache.addAll(FILES_TO_CACHE);
+      cache.addAll(FILES_TO_CACHE);
     })
   );
   self.skipWaiting();
 });
-
-// self.addEventListener("install", function (evt) {
-//   evt.waitUntil(
-//     caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
-//   );
-//   evt.waitUntil(
-//     caches.open(CACHE_NAME).then((cache) => {
-//       cache.addAll(FILES_TO_CACHE);
-//     })
-//   );
-//   self.skipWaiting();
-// });
 
 self.addEventListener("activate", function (evt) {
   evt.waitUntil(
